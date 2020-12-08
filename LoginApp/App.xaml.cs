633e9ec4a6d5012fb7388a6e1454913bf9f7c05c
Justin Martin <33104455+Ekl1p5e@users.noreply.github.com>
2020-12-07 19:09:33 -0600

    Initial commit

---
+++ LoginApp/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+
+namespace LoginApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            this.StartupUri = new Uri(@"Views\MainWindow.xaml", UriKind.Relative);
+
+            base.OnStartup(e);
+        }
+    }
+}
